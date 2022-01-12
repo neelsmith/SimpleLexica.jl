479dc9f75fd45d9d581d7b7a49a40db33e5d075c
Neel Smith <dnsmith.neel@gmail.com>
2022-01-12 15:04:16 -0500

    Do cex trait

--- src/lexicon.jl
+++ src/lexicon.jl
@@ -24,14 +24,35 @@ function citablecollectiontrait(::Type{Lexicon})
     CitableLexicon()
 end
 
+"Singleton type for value of `CexTrait`"
 struct LexiconCex <: CexTrait end
+
+"""Set value of `CexTrait` for type `Lexicon`.
+$(SIGNATURES)
+"""
 function cextrait(::Type{Lexicon})
     LexiconCex()
 end
 
+"""Format `lexicon` as delimited text.
+$(SIGNATURES)
+"""
+function cex(lexicon::Lexicon; delimiter = "|")
+end
 
+
+"""Instantiate a `Lexicon` from delimited text.
+$(SIGNATURES)
+When `strict` is true, uses URN filtering to extract lexicon
+data from CEX source.  When `strict` is false, the assumption is
+that you are using one of Christopher Blackwell's CEX sources, and you
+have extracted a single CEX block of lines to parse.
+"""
 function fromcex(trait::LexiconCex, cexsrc::AbstractString, T;
     delimiter = "|", configuration = nothing, strict = true)
+    if strict
+        @warn("Strict parsing not yet implemented.")
+    end
     entries = []
     for ln in split(cexsrc, "\n")[2:end]
         if !isempty(ln)
