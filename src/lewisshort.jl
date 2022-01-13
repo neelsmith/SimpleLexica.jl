82774ffa00fdb122138e6d858dab4be90e691580
Neel Smith <dnsmith.neel@gmail.com>
2022-01-13 05:45:49 -0500

    Add HTTP

--- src/lewisshort.jl
+++ src/lewisshort.jl
@@ -1,5 +1,15 @@
 LEWIS_SHORT_URL = "https://raw.githubusercontent.com/Eumaeus/cex_lewis_and_short/master/ls.cex"
 
 
+"""Construct Lewis-Short by downloading CEX from Christopher Blackwell's github repository.
+$(SIGNATURES)
+"""
 function lewis_short()
+    
+end
+
+"""Construct Lewis-Short lexicon from a string of delimited text.
+$(SIGNATURES)
+"""
+function lewis_short(s::AbstractString)
 end
