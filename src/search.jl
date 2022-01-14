863ad88e40e86aab2b26ae7d6fa7e8f3a500261e
Neel Smith <dnsmith.neel@gmail.com>
2022-01-14 10:26:28 -0500

    Index search

--- src/search.jl
+++ src/search.jl
@@ -5,23 +5,26 @@
 """Search lexicon for string.
 $(SIGNATURES)
 """
-function search(lex::Lexicon, s::AbstractString, searchtype::SearchType = ALL; 
+function search(lex::Lexicon, s::AbstractString;  searchtype::SearchType = ALL, 
     simplified = nothing, case_sensitive = true)
-    
     searchable = isnothing(simplified) ? simplify(lex, case_sensitive = case_sensitive) : simplified.entries
     query = case_sensitive ? s : lowercase(s)
-    results = if searchtype == LEMMA  
-        filter(entry -> contains(lemma(entry), query), searchable) 
+   
+    results = []
+    if searchtype == LEMMA  
+        for idx in findall(a -> contains(lemma(a), query), searchable)
+            push!(results, lex.entries[idx])
+        end
     elseif searchtype == ARTICLE
-        filter(entry -> contains(article(entry), query), searchable)
+        for idx in findall(a -> contains(article(a), query), searchable)
+            push!(results, lex.entries[idx])
+        end
     else
-        filter(entry -> contains(lemma(entry), query) || contains(article(entry), query), searchable)
+        for idx in findall(a -> contains(article(a), query ) || contains(lemma(a), query), searchable)
+            push!(results, lex.entries[idx])
+        end
     end
-    formatted = []
-    for res in results
-        push!(formatted, lookup(urn(res), lex))
-    end
-    formatted
+    results |> Lexicon
 end
 
 
